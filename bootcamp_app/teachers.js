e2c425f2d07e0b0dfb0bfd1511db235f4a313c8d
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-09 23:48:23 -0500

    Implement parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,19 @@
 const { Pool } = require('pg');
 const args = process.argv;
 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohorts
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+const cohortName = args[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,15 +21,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohorts
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[2]}'
-ORDER BY teacher;
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohorts}: ${user.teacher}`);
