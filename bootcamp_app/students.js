18be3afd5b3730f60d9ac847f4008ef95c6959b0
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-09 23:48:17 -0500

    Implement parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,18 @@
 const { Pool } = require('pg');
 const args = process.argv;
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = args[2];
+const limit = args[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,13 +20,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${args[2]}%'
-LIMIT ${args[3]};
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
